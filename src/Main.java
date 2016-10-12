e524ef1b639faba81a46bf61571b91869340687b
Ricardo <nespacontrib@gmail.com>
2016-10-12 04:29:23 +0100

    Implement simple Floyd-Warshall algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,94 @@
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main
 {
-	public static void main(String[] args)
-	{
-		Scanner reader = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        Scanner reader = new Scanner(System.in);
 
-		int n = reader.nextInt();
-		int mat[][] = new int[n][n];
+        IntMatrix mat = new IntMatrix(reader);
 
-		for (int i = 0; i < n; ++i)
-		{
-			for (int j = 0; j < n; ++j)
-				mat[i][j] = reader.nextInt();
-		}
+        System.out.print(mat.ToString());
 
-		String s = String.format("n = %d", n);
-		System.out.println(s);
+        System.out.print(mat.CalcFloydWarshallSimple().ToString());
+    }
+}
 
-		for (int i = 0; i < n; ++i)
-		{
-			s = "";
-			for (int j = 0; j < n; ++j)
-				s += String.format("%d ", mat[i][j]);
+class IntMatrix
+{
+    private int _n;
+    private int _mat[][];
 
-			System.out.println(s);
-		}
-	}
+    public IntMatrix(Scanner scanner)
+    {
+        _n = scanner.nextInt();
+        _mat = new int[_n][_n];
+
+        for (int i = 0; i < _n; ++i)
+        {
+            for (int j = 0; j < _n; ++j)
+                _mat[i][j] = scanner.nextInt();
+        }
+    }
+
+
+    public String ToString()
+    {
+        String s = String.format("n = %d\n", _n);
+
+        for (int i = 0; i < _n; ++i)
+        {
+            for (int j = 0; j < _n; ++j)
+                s += String.format("%d ", _mat[i][j]);
+
+            s += "\n";
+        }
+
+        return s;
+    }
+
+    public IntMatrix CalcFloydWarshallSimple()
+    {
+        // non-matrix Floyd Warshall requires:
+        // _mat[i][i] == 0
+        // - infinite cost for unconnected vertexes
+        int infinity = 20000; // Integer.MAX_VALUE leads to overflows
+
+        for (int i = 0; i < _n; ++i)
+        {
+            for (int j = 0; j < _n; ++j)
+            {
+                if (i == j)
+                    assert(_mat[i][j] == 0);
+                else if (_mat[i][j] == 0)
+                    _mat[i][j] = infinity;
+            }
+        }
+
+        for (int k = 0; k < _n; ++k)
+        {
+            for (int i = 0; i < _n; ++i)
+            {
+                for (int j = 0; j < _n; ++j)
+                {
+                    if (_mat[i][j] > _mat[i][k] + _mat[k][j])
+                        _mat[i][j] = _mat[i][k] + _mat[k][j];
+                }
+            }
+        }
+
+        // turn back 'infinite cost' into 0
+        for (int i = 0; i < _n; ++i)
+        {
+            for (int j = 0; j < _n; ++j)
+            {
+                if (_mat[i][j] >= infinity)
+                    _mat[i][j] = 0;
+            }
+        }
+
+        return this;
+    }
 }
