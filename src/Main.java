dd372b87d028d43747fe474ab5d89957bd9ca3a2
Ricardo <nespacontrib@gmail.com>
2016-10-12 05:42:33 +0100

    Implement matrix Floyd-Warshall algorithm

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,11 @@ public class Main
 
         IntMatrix mat = new IntMatrix(reader);
 
+        System.out.println("[INITIAL MATRIX]");
         System.out.print(mat.ToString());
 
-        System.out.print(mat.CalcFloydWarshallSimple().ToString());
+        System.out.println("[FINAL MATRIX]");
+        System.out.print(mat.CalcMatrixFloydWarshall().ToString());
     }
 }
 
@@ -20,6 +22,7 @@ class IntMatrix
 {
     private int _n;
     private int _mat[][];
+    private final static int INFINITY = 20000; // Integer.MAX_VALUE leads to overflows
 
     public IntMatrix(Scanner scanner)
     {
@@ -49,23 +52,9 @@ class IntMatrix
         return s;
     }
 
-    public IntMatrix CalcFloydWarshallSimple()
+    public IntMatrix CalcSimpleFloydWarshall()
     {
-        // non-matrix Floyd Warshall requires:
-        // _mat[i][i] == 0
-        // - infinite cost for unconnected vertexes
-        int infinity = 20000; // Integer.MAX_VALUE leads to overflows
-
-        for (int i = 0; i < _n; ++i)
-        {
-            for (int j = 0; j < _n; ++j)
-            {
-                if (i == j)
-                    assert(_mat[i][j] == 0);
-                else if (_mat[i][j] == 0)
-                    _mat[i][j] = infinity;
-            }
-        }
+        PrepareForFloydWarshall();
 
         for (int k = 0; k < _n; ++k)
         {
@@ -79,16 +68,68 @@ class IntMatrix
             }
         }
 
+        PrepareForOutput();
+        return this;
+    }
+
+    public IntMatrix CalcMatrixFloydWarshall()
+    {
+        PrepareForFloydWarshall();
+
+        int iterations = (int)Math.ceil(Math.log(_n) / Math.log(2));
+
+        // each iteration will generate paths of length 2^itr
+        // last iteration must have paths of length _n
+        assert(Math.pow(2, iterations) >= _n);
+
+        for (int itr = 0; itr < iterations; ++itr)
+        {
+            // just a matrix multiplication by self
+            for (int i = 0; i < _n; ++i)
+            {
+                for (int j = 0; j < _n; ++j)
+                {
+                    int result = _mat[i][j];
+                    for (int rowCol = 0; rowCol < _n; ++rowCol)
+                        result = Math.min(result, _mat[rowCol][j] + _mat[i][rowCol]);
+
+                    _mat[i][j] = result;
+                }
+            }
+        }
+
+        PrepareForOutput();
+        return this;
+    }
+
+    private void PrepareForFloydWarshall()
+    {
+        // Floyd Warshall requires:
+        // _mat[i][i] == 0
+        // - infinite cost for unconnected vertexes
+
+        for (int i = 0; i < _n; ++i)
+        {
+            for (int j = 0; j < _n; ++j)
+            {
+                if (i == j)
+                    assert(_mat[i][j] == 0);
+                else if (_mat[i][j] == 0)
+                    _mat[i][j] = INFINITY;
+            }
+        }
+    }
+
+    private void PrepareForOutput()
+    {
         // turn back 'infinite cost' into 0
         for (int i = 0; i < _n; ++i)
         {
             for (int j = 0; j < _n; ++j)
             {
-                if (_mat[i][j] >= infinity)
+                if (_mat[i][j] >= INFINITY)
                     _mat[i][j] = 0;
             }
         }
-
-        return this;
     }
 }
